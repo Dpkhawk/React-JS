7e69135a59646fdfb9fe2f1faa26a2c1536833ff
Dpkhawk <deepak.periyasamy@aspiresys.com>
2024-02-09 17:59:36 +0530

    09/02/2024

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Introduction from "./Feb 8/Introduction";
+//import { DisplayDetails } from "./Feb 8/Introduction";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    return (<>
+    <Introduction/>
+    {/* <DisplayDetails obj={{name:"deepak",
+                        array:[1,2,3,4]}}/> */}
+    </>)
 }
-
-export default App;
